f7f894dcedd5e3a7a78db97f59185a5f4745ba29
Vladislav <vladislav-simf@yandex.ru>
2021-03-12 16:38:51 +0300

    Add table for points

--- app/models.py
+++ app/models.py
@@ -1,10 +1,13 @@
 from app import db
+from sqlalchemy_serializer import SerializerMixin
 
-class MapPoints(db.Model):
+class MapPoints(db.Model, SerializerMixin):
+    serialize_only = ('id', 'name', 'pointX', 'pointY')
+
     id = db.Column(db.Integer, primary_key=True)
-    pointX = db.Column(db.Float, index=True, unique=True)
-    pointY = db.Column(db.Float, index=True, unique=True)
-    name = db.Column(db.String(120), index=True, unique=True)
+    name = db.Column(db.String(120), index=True, unique=False)
+    pointX = db.Column(db.Float, index=True, unique=False)
+    pointY = db.Column(db.Float, index=True, unique=False)
 
     def __repr__(self):
-        return '<User {}>'.format(self.name) 
+        return '<Sightseen name {}, pointX {}, pointY {}>'.format(self.name, self.pointX, self.pointY) 
--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,13 @@
-from app import app
-from flask import render_template
+from app import app, db
+from flask import render_template, jsonify
+from app.models import MapPoints
 
 @app.route('/')
 @app.route('/index')
 def index():
-    user = {'username': 'Vladislav'}
-    return render_template('index.html', title='Home', user=user)
+    point = MapPoints.query.all()
+    db.session.add(point)
+    db.session.commit()
+    return point.to_dict()
+    # user = {'username': 'Vladislav'}
+    # return render_template('index.html', title='Home', user=user)
