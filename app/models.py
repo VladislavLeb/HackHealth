1b1c831ed03616847d18f405d8ca8af237887cff
Vladislav <vladislav-simf@yandex.ru>
2021-03-13 15:46:59 +0300

    add maps

--- app/models.py
+++ app/models.py
@@ -2,12 +2,45 @@ from app import db
 from sqlalchemy_serializer import SerializerMixin
 
 class MapPoints(db.Model, SerializerMixin):
-    serialize_only = ('id', 'name', 'pointX', 'pointY')
+    serialize_only = ('id', 'name', 'city', 'pointX', 'pointY')
 
     id = db.Column(db.Integer, primary_key=True)
+    city = db.Column(db.String(120), index=True, unique=False)
     name = db.Column(db.String(120), index=True, unique=False)
     pointX = db.Column(db.Float, index=True, unique=False)
     pointY = db.Column(db.Float, index=True, unique=False)
 
     def __repr__(self):
-        return '<Sightseen name {}, pointX {}, pointY {}>'.format(self.name, self.pointX, self.pointY) 
+        return '<Sightseen name {}, city {}, pointX {}, pointY {}>'.format(self.name, self.city, self.pointX, self.pointY) 
+
+    @property
+    def serialize(self):
+       """Return object data in easily serializable format"""
+       return {
+           'id'         : self.id,
+           'city'       : self.city,
+           'name'       : self.name,
+           'pointX'     : self.pointX,
+           'pointY'     : self.pointY
+       }
+
+class MapRoutes(db.Model, SerializerMixin):
+    serialize_only = ('id', 'city', 'routeName', 'mapPointNames')
+
+    id = db.Column(db.Integer, primary_key=True)
+    city = db.Column(db.String(120), index=True, unique=False)
+    routeName = db.Column(db.String(120), index=True, unique=False)
+    mapPointNames = db.Column(db.String(400), index=True, unique=False)
+
+    def __repr__(self):
+        return '<Sightseen city {}, routeName {}, mapPointNames {}>'.format(self.city, self.routeName, self.mapPointNames) 
+
+    @property
+    def serialize(self):
+       """Return object data in easily serializable format"""
+       return {
+           'id'             : self.id,
+           'city'           : self.city,
+           'routeName'      : self.routeName,
+           'mapPointNames'  : self.mapPointNames
+       }
--- app/routes.py
+++ app/routes.py
@@ -1,13 +1,90 @@
 from app import app, db
-from flask import render_template, jsonify
-from app.models import MapPoints
+from flask import render_template, jsonify, request
+import json
+from app.models import MapPoints, MapRoutes
 
 @app.route('/')
 @app.route('/index')
 def index():
-    point = MapPoints.query.all()
-    db.session.add(point)
-    db.session.commit()
-    return point.to_dict()
-    # user = {'username': 'Vladislav'}
-    # return render_template('index.html', title='Home', user=user)
+    # point = MapPoints(name = "Аптека", city = 'п. Глуша (Бобруйский р-н)', pointX = 53.087330, pointY = 28.861244)
+    # db.session.add(point)
+    # point = MapPoints(name = "Памятник Алесю Адамовичу", city = 'п. Глуша (Бобруйский р-н)', pointX = 53.087486, pointY = 28.854656)
+    # db.session.add(point)
+    # point = MapPoints(name = "Могила Алеся Адамовича", city = 'п. Глуша (Бобруйский р-н)', pointX = 53.087486, pointY = 28.854656)
+    # db.session.add(point)
+    # point = MapPoints(name = "Могила матери Алеся Адамовича", city = 'п. Глуша (Бобруйский р-н)', pointX = 53.087486, pointY = 28.854656)
+    # db.session.add(point)
+    # point = MapPoints(name = "Мельница", city = 'п. Глуша (Бобруйский р-н)', pointX = 53.091687, pointY = 28.865772)
+    # db.session.add(point)
+    # point = MapPoints(name = "Старообрядческая церковь", city = 'п. Глуша (Бобруйский р-н)', pointX = 53.091200, pointY = 28.866491)
+    # db.session.add(point)
+    # point = MapPoints(name = "Гончарная", city = 'п. Глуша (Бобруйский р-н)', pointX = 53.091365, pointY = 28.865078)
+    # db.session.add(point)
+    # point = MapPoints(name = "арт-объект Прыпынак Адамовича", city = 'п. Глуша (Бобруйский р-н)', pointX = 53.087824, pointY = 28.865188)
+    # db.session.add(point)
+
+    # newRoute = MapRoutes(city = 'п. Глуша (Бобруйский р-н)', routeName = 'Жизнь Алеся Адамовича', mapPointNames = 'арт-объект Прыпынак Адамовича, Могила матери Алеся Адамовича, Могила Алеся Адамовича, Памятник Алесю Адамовичу, Аптека')
+    # db.session.add(newRoute)
+    # newRoute = MapRoutes(city = 'п. Глуша (Бобруйский р-н)', routeName = 'Музей ремесел', mapPointNames = 'Гончарная, Старообрядческая церковь, Мельница')
+    # db.session.add(newRoute)
+    # db.session.commit()
+    # return jsonify(json_list=[i.serialize for i in MapRoutes.query.all()])
+    # return jsonify(json_list = MapPoints.query.all())
+    user = {'username': 'Vladislav'}
+    return render_template('index.html', title='Home', user=user)
+
+@app.route('/select-route')
+def selectRoute():
+    # routesJson = [
+    #     {
+    #         'author': {'username': 'John'},
+    #         'body': 'Beautiful day in Portland!'
+    #     },
+    #     {
+    #         'author': {'username': 'Susan'},
+    #         'body': 'The Avengers movie was so cool!'
+    #     }, 
+    #     {
+    #         'author': {'username': 'Ипполит'},
+    #         'body': 'Какая гадость эта ваша заливная рыба!!'
+    #     }
+    # ]
+    routes = [i.serialize for i in MapRoutes.query.all()]
+    return render_template('selectRoute.html', title='Выбор маршрута', routes= routes)
+    # return jsonify(json_list=[i.serialize for i in MapRoutes.query.all()])
+
+@app.route('/select-point')
+def selectPoint():
+    points = [i.serialize for i in MapPoints.query.all()]
+    return render_template('selectPoint.html', title='Выбор точки', points = points)
+
+@app.route('/build-route', methods=['GET'])
+def buildRoute():
+    pointList = []
+    routeId = request.args.get('routeId')
+    route = MapRoutes.query.get(routeId)
+    pointNamesList = route.mapPointNames.split(",")
+    rtt = 'pedestrian'
+    url = 'https://yandex.ru/map-widget/v1/?rtext='
+    for pointName in pointNamesList:
+        point = MapPoints.query.filter_by(name=pointName.strip()).first()
+        pointList.append([point.pointX, point.pointY])
+        url = url + str(point.pointX) + "%2C" + str(point.pointY) + "~"
+    url = url[:len(url) - 1]
+    url = url + "&rtt=" + rtt
+    return render_template('map.html', url = url)
+
+@app.route('/goto-point', methods=['GET'])
+def gotoPoint():
+    zoom = '14'
+    pointId = request.args.get('pointId')
+    point = MapPoints.query.get(pointId)
+    url = 'https://yandex.ru/map-widget/v1/?rtext='
+    url = url + str(point.pointX) + "%2C" + str(point.pointY) + "~"
+    url = url[:len(url) - 1]
+    url = url + "&z=" + zoom
+    return render_template('map.html', url = url)
+
+@app.route('/map', methods=['GET'])
+def openMap():
+    return render_template('map.html')
